54caa413974a82aa77f52f126ab5a56378385273
seethakris <seethakris@gmail.com>
2018-02-21 20:57:45 -0600

    Script for running expriment

---
+++ StartExperiment.py
@@ -0,0 +1,133 @@
+import os
+import cv2
+import pandas as pd
+import csv
+import numpy as np
+from psychopy import core
+import sys
+from copy import copy
+
+
+class Experiment(object):
+    def __init__(self, NumberOfTrials, RewardTimeOut, Map, RewardLocationCSV, ExperimentParamFolder):
+        """ Initialise user input
+                Params :
+                NumberOfTrials : number of trials in experiment
+                RewardTimeout : number of seconds after which the trial ends and no reward is given
+                Map : map of the area stored as .pgm
+                RewardLocationCSV : csv file that contains reward locations
+                ExperimentParamFolder : folder to save experiment related parameters
+
+        """
+        self.ExperimentParamFolder = ExperimentParamFolder
+        cv2.namedWindow('CheckRewardLocation', cv2.WINDOW_NORMAL)
+        self.map = cv2.imread(Map)
+        print(np.shape(self.map))
+        self.rewardlocation = pd.read_csv(RewardLocationCSV, index_col=0)
+        print 'Number of saved reward locations loaded : ', len(self.rewardlocation)
+        self.plot_saved_location()
+
+        # Initialise Trial parameters
+        self.numtrials = NumberOfTrials
+        self.rewardtimeout = RewardTimeOut
+        self._currenttrial = 0  # Trial number
+        self.start_experiment()
+
+    def plot_saved_location(self):
+        """ Check if the saved reward locations are correct"""
+        locationmap = copy(self.map)
+        for index, row in self.rewardlocation.iterrows():
+            cv2.circle(locationmap, (row['x'], row['y']), 20, (0, 0, 255), -1)
+            cv2.putText(locationmap, '(%d, %d)' % (row['x'], row['y']), (row['x'], row['y']), cv2.FONT_HERSHEY_SIMPLEX,
+                        2, (0, 0, 0), 4, cv2.LINE_AA)
+        print('Press Esc if reward locations are correct')
+        while True:
+            cv2.imshow('CheckRewardLocation', locationmap)
+            if cv2.waitKey(20) & 0xFF == 27:
+                break
+        cv2.imwrite(os.path.join(self.ExperimentParamFolder, 'RewardLocationsused.tif'), locationmap)
+        cv2.destroyAllWindows()
+
+    def updatetrial(self):
+        """ Update trial """
+        self._currenttrial += 1
+
+    def start_experiment(self):
+        """ Display a reward location and start timer"""
+        while self._currenttrial < self.numtrials:
+            print ('Trial %d beginning...' % self._currenttrial)
+            currentreward = np.random.choice(len(self.rewardlocation), 1)
+            currentrewardlocation = self.rewardlocation.iloc[currentreward]
+
+            # Start trial - show reward
+            print ('Go to reward location (%d, %d)' % (currentrewardlocation['x'], currentrewardlocation['y']))
+            self.plotrewardlocation(currentrewardlocation)
+
+            # Start timer for reward timeout
+            timer = core.CountdownTimer(self.rewardtimeout)
+            abstime = core.MonotonicClock()
+            while timer.getTime() > 0:
+                # Check Location of animal - link to the callback function here
+
+                # Print elapsed time
+                elapsedtime = abstime.getTime()
+                print "\033[K Elapsed Time : ", elapsedtime, "\r",
+                sys.stdout.flush()
+
+            print('\n Trial Ended')
+            self.updatetrial()  # Trial ended - start of next trial
+
+    def plotrewardlocation(self, rewardloc):
+        """ Plot the reward location on the Map"""
+        locationmap = copy(self.map)
+        cv2.namedWindow('Trial %d Reward' % self._currenttrial, cv2.WINDOW_NORMAL)
+        cv2.circle(locationmap, (rewardloc['x'], rewardloc['y']), 50, (0, 0, 255), -1)
+        cv2.imshow('Trial %d Reward' % self._currenttrial, locationmap)
+        cv2.waitKey(5000)  # Show plot for 5 seconds
+        cv2.destroyAllWindows()
+
+    def get_random_xy_loc(self):
+        x = np.random.randint(np.size(self.map, 1))
+        y = np.random.randint(np.size(self.map, 0))
+
+        return x, y
+
+
+def SaveExpParameters(ExperimentParamFolder, **kwargs):
+    with open(os.path.join(ExperimentParamFolder, 'ExperimentParameters.csv'), 'wb') as f:  # Just use 'w' mode in 3.x
+        w = csv.writer(f)
+        for key, value in kwargs.items():
+            w.writerow([key, value])
+
+
+if __name__ == '__main__':
+    LocationMap = '/Users/seetha/Desktop/MonkeyRewardTask/SampleData/map1.pgm'
+    RewardLocationCSV = '/Users/seetha/Desktop/MonkeyRewardTask/SampleData/rewardlocations.csv'
+
+    # Define Experiment Parameters here
+    ExperimentNumber = 1
+    ExperimentDate = '21022018'
+    MonkeyName = 'Chimpian'
+    NumberOfTrials = 10
+    RewardTimeOut = 10  # In seconds
+    ExperimentFolder = '/Users/seetha/Desktop/MonkeyRewardTask/SampleData/'  # Main folder for saving related data
+
+    # Create a folder to save Experiment related parameters
+    SaveDataFolder = os.path.join(ExperimentFolder, ExperimentDate, str(ExperimentNumber) + '_' + MonkeyName)
+    if not os.path.exists(SaveDataFolder):
+        os.makedirs(SaveDataFolder)
+
+    # Save Experiment parameters
+    SaveExpParameters(SaveDataFolder,
+                      ExperimentNumber=ExperimentNumber,
+                      ExperimentDate=ExperimentDate,
+                      MonkeyName=MonkeyName,
+                      NumberOfTrials=NumberOfTrials,
+                      RewardTimeOut=RewardTimeOut)
+
+    # Start Experiment
+    Experiment(NumberOfTrials=10,
+               RewardTimeOut=10,
+               Map=LocationMap,
+               RewardLocationCSV=RewardLocationCSV,
+               ExperimentParamFolder=SaveDataFolder)
